291bde8ac4be6c993b269711cf7e10d85596ae8b
Anton Pisarenko <antpisar@gmail.com>
2023-02-19 19:03:20 +0300

    updates

--- src/app/viewer/directives/annotation-move.directive.ts
+++ src/app/viewer/directives/annotation-move.directive.ts
@@ -1,13 +1,49 @@
-import { Directive, HostBinding } from '@angular/core';
+import { Annotation } from 'src/app/viewer/annotations/annotation';
+import { Directive, HostBinding, HostListener, Input, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appAnnotationMove]'
 })
 export class AnnotationMoveDirective {
 
+  @Input() annotation: Annotation;
+
+  private isMoving = false;
+  private offsetX: number;
+  private offsetY: number;
+
+  constructor(
+    private elRef: ElementRef,
+  ) { }
+
   @HostBinding('style.cursor')
-  backgroundColor: string = 'move';
+  get cursor(): string {
+    if (this.annotation.completed) {
+      return 'move';
+    }
 
-  constructor() { }
+    return 'default';
+  }
 
+  @HostListener('mousedown', ['$event'])
+  mouseDown(event: MouseEvent) {
+    event.preventDefault();
+    this.isMoving = true;
+
+    this.offsetX = this.elRef.nativeElement.offsetLeft - event.clientX;
+    this.offsetY = this.elRef.nativeElement.offsetTop - event.clientY;
+  }
+
+  @HostListener('document:mousemove', ['$event'])
+  mouseMove(event: MouseEvent) {
+    if (this.isMoving) {
+      this.annotation.coords.x = event.clientX + this.offsetX;
+      this.annotation.coords.y = event.clientY + this.offsetY;
+    }
+  }
+
+  @HostListener('document:mouseup')
+  mouseUp() {
+    this.isMoving = false;
+  }
 }
